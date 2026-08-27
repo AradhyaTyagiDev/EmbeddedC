697d1e7d263d56761e204f20255f564c61e517c8
kuldeep <kuldeep.tyagi.vestel@gmail.com>
2026-08-27 13:33:15 +0530

    Start Commit

---
+++ BasicSyntaxC/src/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int result = a + b;
+
+    return result;
+}
+
+int main(void)
+{
+    int x = 10;
+    int y = 20;
+
+    int sum = add(x, y);
+
+    printf("x = %d\n", x);
+    printf("y = %d\n", y);
+    printf("sum = %d\n", sum);
+    printf("Hello, World!\n");
+
+    return 0;
+}
