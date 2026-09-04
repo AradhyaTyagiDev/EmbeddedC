51799e5c031e18635ac1a2ca8737b905853d6e08
kuldeep <kuldeep.tyagi.vestel@gmail.com>
2026-09-04 17:16:23 +0530

    Pointer arthmatic

--- BasicSyntaxC/src/main.c
+++ BasicSyntaxC/src/main.c
@@ -3,13 +3,16 @@
 
 int main(void) {
 
-    int x = 10;
-     int *ptr = &x;
-     checkForNullPointer(ptr);
+    int value = 10;
+    int *ptr1 = &value;
+    int *ptr2 ;
+    ptr2 = ptr1 - 1;
+    // *ptr2 = 20;
 
-    printf("x address %p\n", &x);
-    printf("pointer  %p\n", ptr);
-    printf("pointer value %p\n", &ptr);
+    printf("ptr1: %p\n", ptr1);
+    printf("ptr2: %p\n", ptr2);
+    printf("ptr1 points to value: %d\n", *ptr1);
+    printf("ptr2 points to value: %d\n", *ptr2);
 
     return 0;
 }
